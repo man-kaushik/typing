efa9ff729b3f7b583043146a594dc14d9b369681
Mannu <ms4055028@gmail.com>
2024-10-09 23:46:41 +0530

    Typing application changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText= '';
+  // matchingText:boolean=false;
+
+  onInput(eve:any){
+    // this.matchingText=false;
+    // if(eve.target.value==this.randomText){
+    //   this.matchingText=true;
+    // }
+    this.enteredText=eve.target.value;
+  }
+  compare(randomLetter:string,enteredLetter:string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+    return randomLetter==enteredLetter? 'correct' : 'incorrect';
+  }
+
 }
